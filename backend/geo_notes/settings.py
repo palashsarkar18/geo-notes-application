b3af1213c7280a31f09320ab89c7f50c5a829aa3
Palash Sarkar <palash.sarkar@protonmail.com>
2024-06-14 10:49:44 +0300

    Run backend container with gunicorn

--- backend/geo_notes/settings.py
+++ backend/geo_notes/settings.py
@@ -91,24 +91,14 @@ WSGI_APPLICATION = 'geo_notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB'),
-#         'USER': env('POSTGRES_USER'),
-#         'PASSWORD': env('POSTGRES_PASSWORD'),
-#         'HOST': env('POSTGRES_SERVER'),
-#         'PORT': env('POSTGRES_PORT'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geo_notes_db',
-        'USER': 'geo_user',
-        'PASSWORD': 'geo_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_SERVER'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
 
